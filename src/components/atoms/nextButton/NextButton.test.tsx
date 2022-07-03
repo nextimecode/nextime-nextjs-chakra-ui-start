28b16df018d299274fbd2e0fc9197b736242504d
phdduarte <pedrohlduarte@gmail.com>
2022-07-03 15:12:41 -0300

    feat: add yarn test in build

--- src/components/atoms/nextButton/NextButton.test.tsx
+++ src/components/atoms/nextButton/NextButton.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import NextButton from '../NextButton/index'
+import NextButton from './index'
 
 describe('NextButton', () => {
   it('renders correctly', () => {
