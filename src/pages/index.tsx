cc7de9316b1bc61ef083656a3afb0b806fb51d72
Pedro Duarte <pedrohlduarte@gmail.com>
2022-08-24 21:01:18 -0300

    feat: setup do apollo e graphl

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,19 @@ import React from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import NextButton from '../components/atoms/nextButton'
+import { gql, useQuery } from '@apollo/client'
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
 
 export default function Home() {
+  const { data } = useQuery(GET_LESSONS_QUERY)
   return (
     <div>
       <Head>
