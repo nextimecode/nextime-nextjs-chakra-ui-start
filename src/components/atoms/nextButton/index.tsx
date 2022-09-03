fdd946f90fd7dde7c5906aa59951bb60250c9542
Pedro Duarte <pedrohlduarte@gmail.com>
2022-09-03 19:12:16 -0300

    fix: importacao do nextbutton

--- src/components/atoms/nextButton/index.tsx
+++ src/components/atoms/nextButton/index.tsx
@@ -1,10 +1,10 @@
 import React, { PropsWithChildren } from 'react'
 import { Button } from '@chakra-ui/react'
 
-type Props = {
+interface NextButtonProps {
   bg?: 'next-blue.400' | 'next-dark.200' | undefined
 }
 
-export function NextButton({ bg = 'next-blue.400', children }: PropsWithChildren<Props>) {
+export function NextButton({ bg = 'next-blue.400', children }: PropsWithChildren<NextButtonProps>) {
   return <Button bg={bg}>{children}</Button>
 }
